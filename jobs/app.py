fbcdf92924d9894968e153937101f08454d0fb83
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-07-01 14:16:13 +0200

    update new routes

---
+++ jobs/app.py
@@ -0,0 +1,16 @@
+from flask import (
+    Flask,
+    g,
+    render_template,
+    flash,
+    redirect,
+    url_for,
+    abort
+)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def jobs():
+    return render_template('index.html')
