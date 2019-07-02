567bead0df9d5eb5f747a9672add94fa65f29ced
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-07-02 12:04:59 +0200

    updates

--- jobs/app.py
+++ jobs/app.py
@@ -58,11 +58,11 @@ def job(job_id):
 @app.route('/employer/<employer_id>')
 def employer(employer_id):
     employer_data = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single=True)
-    employer_jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary '
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary '
                                 'FROM job JOIN employer ON employer.id = job.employer_id '
                                 'WHERE employer.id = ?', [employer_id])
     reviews = execute_sql('SELECT review, rating, title, date, status '
                           'FROM review JOIN employer ON employer.id = review.employer_id '
                           'WHERE employer.id = ?', [employer_id])
-    return render_template('employer.html', employer=employer_data, jobs=employer_jobs,
+    return render_template('employer.html', employer=employer_data, jobs=jobs,
                            reviews=reviews)
