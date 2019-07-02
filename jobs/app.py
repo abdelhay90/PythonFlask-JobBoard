c7119af1fcb55919dc86870c549dc7ee1088c109
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-07-02 12:37:58 +0200

    updates

--- jobs/app.py
+++ jobs/app.py
@@ -74,12 +74,12 @@ def employer(employer_id):
 @app.route('/employer/<employer_id>/review', methods=('GET', 'POST'))
 def review(employer_id):
     if request.method == 'POST':
-        review_ = request.form['review']
+        review = request.form['review']
         rating = request.form['rating']
         title = request.form['title']
         status = request.form['status']
         date = datetime.datetime.now().strftime("%m/%d/%Y")
         execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',
-                    (review_, rating, title, date, status, employer_id), commit=True)
+                    (review, rating, title, date, status, employer_id), commit=True)
         return redirect(url_for('employer', employer_id=employer_id))
     return render_template('review.html', employer_id=employer_id)
